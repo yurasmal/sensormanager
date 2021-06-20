1c69bca41893f6f906b500bfbfca5d55960bd9bb
Hp <yurasmal86@gmail.com>
2021-06-20 23:49:45 +0300

    Initial commit

---
+++ src/main/java/by/ps/sensormanager/SensormanagerApplication.java
@@ -0,0 +1,13 @@
+package by.ps.sensormanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensormanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensormanagerApplication.class, args);
+	}
+
+}
